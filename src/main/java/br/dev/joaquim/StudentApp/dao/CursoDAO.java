ed646fcf39a9896d76d54c5f8d99c3a221787c6c
Allan-cardoso <129688596+Allan-cardoso@users.noreply.github.com>
2024-10-15 21:51:45 -0300

    Update CursoDAO.java

--- src/main/java/br/dev/joaquim/StudentApp/dao/CursoDAO.java
+++ src/main/java/br/dev/joaquim/StudentApp/dao/CursoDAO.java
@@ -1,1 +1,18 @@
+package br.dev.joaquim.StudentApp.dao;
+
+import java.util.List;
+
+import br.dev.joaquim.StudentApp.entities.Curso;
+
+public interface CursoDAO {
+  public boolean create(Curso curso);
+
+  public List<Curso> findAll();
+
+  public Curso findByCodigo(int codigo);
+
+  public boolean update(Curso curso);
+
+  public boolean delete(int codigo);
+}
 
